f48d3d2ac62539ff965e7b3711aa63fea32c0113
loissk <lois.sulki.kim@gmail.com>
2020-09-16 15:36:26 -0700

    Add description and humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'Current temperature is ' + body.current.temperature + '. Feels like ' + body.current.feelslike)
+            callback(
+                undefined, 
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + 
+                body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
