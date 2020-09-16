93586d08cb36ddd38b3762266797744a41b90d4a
loissk <lois.sulki.kim@gmail.com>
2020-09-16 15:24:20 -0700

    Add about text and update name

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
-        name: 'A'
+        name: 'LKim'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'A'
+        name: 'LKim'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text',
         title: 'Help',
-        name: 'A'
+        name: 'LKim'
     })
 })
 
@@ -72,7 +72,7 @@ app.get('/weather', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'A',
+        name: 'LKim',
         errorMessage: 'Help article not found'
     })
 })
@@ -80,7 +80,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'A',
+        name: 'LKim',
         errorMessage: 'Page not found'
     })
 })
